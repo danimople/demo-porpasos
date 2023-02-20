bb9ccfe9284e69c0099e9d10c0f894e2e8fe814f
Danimople <alumno.312059@ies-azarquiel.es>
2023-02-20 16:39:36 +0100

    Sexto commit (Hasta el 03:25) Hemos hecho que se pueda iniciar sesión, y hemos creado el login.js y el AuthController, a parte también hemos añadido métodos a UsuarioDao y UsuarioDaoImp

---
+++ src/main/java/com/example/demoporpasos/controllers/AuthController.java
@@ -0,0 +1,26 @@
+package com.example.demoporpasos.controllers;
+
+import com.example.demoporpasos.dao.UsuarioDao;
+import com.example.demoporpasos.models.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+    @Autowired
+    private UsuarioDao usuarioDao;
+
+    @RequestMapping(value = "api/login", method = RequestMethod.POST)
+    public String login(@RequestBody Usuario usuario) {
+        if (usuarioDao.verificarCredenciales(usuario)){
+            return "OK";
+        }
+        return "FAIL";
+    }
+
+
+}
